ee0b41aad1bad05744eea4238ebe56cdd6d6fe4a
Tovly Deutsch <tovlydeutsch@gmail.com>
2021-01-22 19:34:18 -0500

    removed unused stuff and disabled join when loading

--- src/Game.tsx
+++ src/Game.tsx
@@ -24,6 +24,7 @@ function Game() {
           updateGameWithFunction(addPlayer(name));
           setName(name);
         }}
+        disabled={gameDataLoading}
       />
     );
   }
--- src/JoinGame.tsx
+++ src/JoinGame.tsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 
 type JoinGamePropTypes = {
   onNameSubmit: (name: string) => void;
+  disabled: boolean;
 };
 
-function JoinGame({ onNameSubmit }: JoinGamePropTypes) {
+function JoinGame({ onNameSubmit, disabled }: JoinGamePropTypes) {
   const [name, setName] = useState("");
 
   return (
@@ -19,6 +20,7 @@ function JoinGame({ onNameSubmit }: JoinGamePropTypes) {
         onClick={() => {
           onNameSubmit(name);
         }}
+        disabled={disabled}
       >
         Join
       </button>
